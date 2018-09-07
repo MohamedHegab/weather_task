eb5486e99ec148fe9f5b7a7c8d084cac95880da2
Mohamed Hegab <mohamedhegab92@gmail.com>
2018-09-07 11:14:55 +0200

    configure the application gems and testing

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
+require 'dotenv' ; Dotenv.load ".env"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -21,7 +22,8 @@ module WeatherTask
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.assets.paths << Rails.root.join('vendor', 'assets')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
---
+++ config/initializers/rspec.rb
@@ -0,0 +1,9 @@
+Rails.application.config.generators do |g|
+  g.test_framework :rspec, fixture: true
+  g.fixture_replacement :factory_bot, dir: 'spec/factories'
+  g.view_specs false
+  g.helper_specs false
+  g.stylesheets = false
+  g.javascripts = false
+  g.helper = false
+end
